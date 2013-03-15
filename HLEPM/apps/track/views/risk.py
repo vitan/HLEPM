dbb3a923851001e8228a4eae72a7bc0c201aba36
docai <docai@redhat.com>
2013-03-15 17:06:03 +0800

    REV: bind risk to requirement instance
    Change-Id: Ibf6e93b32474bdbe150843d578ae389c5ef120f4

--- HLEPM/apps/track/views/risk.py
+++ HLEPM/apps/track/views/risk.py
@@ -3,6 +3,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext, loader, Context
 from django.template.loader import render_to_string
@@ -22,11 +23,12 @@ from HLEPM.apps.track.models import RiskStatus
 __all__ = (
     'risk',
     'risk_add',
+    'risk_update',
 )
 #The following is a test function now, will improve it later.
 @login_required
-def risk(request, template_name='track/risk/risk.html'):
-
+def risk(request, app_label, module_name, pk, template_name='track/risk/risk.html'):
+    content_type = ContentType.objects.get(app_label=app_label, model=module_name)
     context_data = {
         'subtitle': 'Risk',
         'Impacts': Impact.objects.all(),
@@ -34,7 +36,10 @@ def risk(request, template_name='track/risk/risk.html'):
         'Probabilities': Probability.objects.all(),
         'statuss': RiskStatus.objects.all(),
         'reporter_url': add_search_url_for_model(User),
-        'risks':Risk.objects.all(),
+        'risks':Risk.objects.filter(content_type=content_type, object_id=pk),
+        'app_label': app_label,
+        'module_name': module_name,
+        'pk': pk,
         }
 
     return render_to_response(template_name, context_data, context_instance=RequestContext(request))
@@ -42,15 +47,18 @@ def risk(request, template_name='track/risk/risk.html'):
 
 @require_POST
 @login_required
-def risk_add(request, template_name='track/risk/display-new-risk.html'):
+def risk_add(request, app_label, module_name, pk, template_name='track/risk/one-risk.html'):
+    content_type = ContentType.objects.get(app_label=app_label, model=module_name)
+    bind_object_data = {'content_type': content_type, 'object_id': pk}
     response = AjaxResponseMixin()
     if request.method == 'POST':
         form = RiskForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
+            data.update(bind_object_data)
             risk_obj = Risk(**data)
             risk_obj.save()
-            new_risk_html = render_to_string(template_name, {'latest_risk': risk_obj})
+            new_risk_html = render_to_string(template_name, {'risk': risk_obj})
             new_risk_data = {'new_risk': new_risk_html}
             return response.ajax_response(new_risk_data)
         else:
@@ -78,9 +86,12 @@ def risk_update(request, risk_id, template_name='track/risk/update-risk.html'):
 
     elif request.method == 'POST':
         response = AjaxResponseMixin()
+        risk_obj = get_object_or_404(Risk, pk=risk_id)
+        bind_object_data = {'content_type': risk_obj.content_type, 'object_id':risk_obj.object_id}
         form = RiskForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
+            data.update(bind_object_data)
             risk_obj = Risk(pk=risk_id, **data)
             risk_obj.save()
             new_template_name = 'track/risk/one-risk.html'
