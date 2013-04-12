de83801c20c4d901c93b1dcf8c3ed0f7efeee948
weizhou <weizhou@redhat.com>
2013-04-12 09:49:05 +0800

    REV: Added home redirect.
    Change-Id: Ifb463752ef2e24773b810c66a6cbb40c586c3a1f

--- HLEPM/urls.py
+++ HLEPM/urls.py
@@ -1,22 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'hlepm.views.home', name='home'),
-    # url(r'^hlepm/', include('hlepm.foo.urls')),
-    url(r'^search/', include('HLEPM.apps.search.urls')),
+    url(r'^$', redirect_to, {'url': '/track/requirement/'}),
     url(r'^track/', include('HLEPM.apps.track.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^search/', include('HLEPM.apps.search.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('HLEPM.apps.comments.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
